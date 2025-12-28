968fee0ec87195be0b9564aa03fe6532a18cf440
Secară Lucas <lucassecara@yahoo.com>
2025-12-28 21:18:18 +0200

    Fixed the issue of the movies not loading
    Switched to a v3 API key, because v4 was deprecated

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,25 +1,20 @@
-const apiKey = import.meta.env.VITE_API_URL;
-const baseUrl = import.meta.env.VITE_BASE_URL;
-const fetchHeaders = {
-  headers: {
-    Authorization: 'Bearer ' + apiKey, accept: 'application/json',
-  },
-}
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+const baseUrl = import.meta.env.VITE_TMDB_BASE_URL;
 
 export async function fetchPopularMovies(page: number) {
-  const res = await fetch(`${baseUrl}/movie/popular?page=${page}`, fetchHeaders)
+  const res = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}&page=${page}`)
   if (!res.ok) throw new Error(`Failed to fetch popular movies: ${res.status}`)
   return res.json()
 }
 
 export async function fetchMovieById(movie_id: number) {
-  const res = await fetch(`${baseUrl}/movie/${movie_id}`, fetchHeaders)
-  if (!res.ok) throw new Error(`Failed to fetch popular movies: ${res.status}`)
+  const res = await fetch(`${baseUrl}/movie/${movie_id}?api_key=${apiKey}`)
+  if (!res.ok) throw new Error(`Failed to fetch movie: ${res.status}`)
   return res.json()
 }
 
 export async function searchMovie(query: string, page: number = 1) {
-  const res = await fetch(`${baseUrl}/search/movie?query=${query}&language=en-US&page=${page}`, fetchHeaders)
-  if (!res.ok) throw new Error(`Failed to fetch popular movies: ${res.status}`)
+  const res = await fetch(`${baseUrl}/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}&page=${page}`)
+  if (!res.ok) throw new Error(`Failed to search movies: ${res.status}`)
   return res.json()
 }
